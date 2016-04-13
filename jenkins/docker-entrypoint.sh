eb6850532ff0ff3b4d3d65ab6761bcdc298ba7f3
Ell Neal <elliot@emdentec.com>
2016-04-13 14:11:43 +0100

    Create the docker group before modifying the user

--- jenkins/docker-entrypoint.sh
+++ jenkins/docker-entrypoint.sh
@@ -2,7 +2,7 @@
 set -e
 
 if [[ -n ${DOCKER_GROUP_ID} ]]; then
-    # groupadd -g ${DOCKER_GROUP_ID} docker
+    sudo groupadd -g ${DOCKER_GROUP_ID} docker
     sudo usermod -a -G ${DOCKER_GROUP_ID} jenkins
 fi
 
